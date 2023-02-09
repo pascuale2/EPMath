065fe0afbb63e154d6b07f9cb263febaed518df9
Erwin Pascual <epas97@hotmail.com>
2023-02-09 10:27:09 -0700

    Add Unit conversion class

---
+++ EPMath/EPMath/UnitConversion.cs
@@ -0,0 +1,8 @@
+﻿namespace EPMath
+{
+    public static class UnitConversion
+    {
+        public const double Deg_To_Rad =  0.0174532925199432;
+        public const double Rad_To_Deg = 57.2957795130823208;
+    }
+}
