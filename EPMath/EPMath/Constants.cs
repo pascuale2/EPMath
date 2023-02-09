6716871026100dae21c50e839ee554e492e12eee
Erwin Pascual <epas97@hotmail.com>
2023-02-08 20:04:47 -0700

    Add Constants class to library

---
+++ EPMath/EPMath/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace EPMath
+{
+    public static class Constants
+    {
+        public const double Tau = 6.2831853071795862;
+        public const double Pi = 3.1415926535897931;
+        public const double E = 2.7182818284590451;
+    }
+}
